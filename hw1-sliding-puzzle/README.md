059ce5045b70f782fefb8ba77d268bfdca874877
Nikolay Babanov <ni.babanov@gmail.com>
2022-10-24 20:54:31 +0300

    HW1 Added initial draft of N Puzzle implementation

--- hw1-sliding-puzzle/README.md
+++ hw1-sliding-puzzle/README.md
@@ -11,10 +11,9 @@ on top, bottom, left and right in place of the empty block.
 At the input is given the number N - the number of blocks with numbers
 (8, 15, 24, etc.), the number I - the index of the position of zero (the empty
 block) in the decision (using -1 the default zero index position is set at the
-bottom right) and then the layout of the board is introduced. Using the A*
-(or IDA*) algorithm and the Manhattan distance heuristics (or Hemming
-distance), derive:
+bottom right) and then the layout of the board is introduced. 
 
+Using the IDA* algorithm and the Manhattan distance heuristics, derive:
 - In the first line, the length of the "optimal" path from start to destination.
 - The appropriate steps (in a new line for each one) that are taken to reach
 the final state. The steps are left, right, up and down
@@ -39,4 +38,48 @@ Output:
 2
 left
 left
+```
+
+
+### IDA* pseudocode:
+
+```ts
+path              current search path (acts like a stack)
+node              current node (last node in current path)
+g                 the cost to reach current node
+f                 estimated cost of the cheapest path (root..node..goal)
+h(node)           estimated cost of the cheapest path (node..goal)
+cost(node, succ)  step cost function
+is_goal(node)     goal test
+successors(node)  node expanding function, expand nodes ordered by g + h(node)
+ida_star(root)    return either NOT_FOUND or a pair with the best path and its cost
+ 
+procedure ida_star(root)
+    bound := h(root)
+    path := [root]
+    loop
+        t := search(path, 0, bound)
+        if t = FOUND then return (path, bound)
+        if t = ∞ then return NOT_FOUND
+        bound := t
+    end loop
+end procedure
+
+function search(path, g, bound)
+    node := path.last
+    f := g + h(node)
+    if f > bound then return f
+    if is_goal(node) then return FOUND
+    min := ∞
+    for succ in successors(node) do
+        if succ not in path then
+            path.push(succ)
+            t := search(path, g + cost(node, succ), bound)
+            if t = FOUND then return FOUND
+            if t < min then min := t
+            path.pop()
+        end if
+    end for
+    return min
+end function
 ```
--- hw1-sliding-puzzle/main.go
+++ hw1-sliding-puzzle/main.go
@@ -1,7 +1,12 @@
 package main
 
-import "math"
-import "fmt"
+import (
+	"container/list"
+	"fmt"
+	"math"
+
+	slices "golang.org/x/exp/slices"
+)
 
 type Point2D struct {
 	X int
@@ -9,9 +14,216 @@ type Point2D struct {
 }
 
 func getManhattenDistance(a Point2D, b Point2D) int {
-	return int(math.Abs(float64(a.X) - float64(b.X))) + int(math.Abs(float64(a.Y) - float64(b.Y)))
+	return int(math.Abs(float64(a.X)-float64(b.X))) + int(math.Abs(float64(a.Y)-float64(b.Y)))
+}
+
+type Node struct {
+	cost       int
+	board      []int
+	pathToNode list.List
+}
+type Heuristic func(root *Node) int
+type GoalPredicate func(node *Node) bool
+
+type SearchResult struct {
+	cost int
+	node *Node
+}
+
+type BoardMove int
+
+const (
+	Top BoardMove = iota
+	Right
+	Bottom
+	Left
+)
+
+var BOARD_MOVE = struct {
+	Top    BoardMove
+	Right  BoardMove
+	Bottom BoardMove
+	Left   BoardMove
+}{Top, Right, Bottom, Left}
+
+func getMoveIndex(boardSide int, currentIndex int, move BoardMove) int {
+	switch move {
+	case BOARD_MOVE.Top:
+		return currentIndex - boardSide
+	case BOARD_MOVE.Bottom:
+		return currentIndex + boardSide
+	case BOARD_MOVE.Left:
+		if currentIndex%boardSide == 0 {
+			return -1
+		}
+		return currentIndex - 1
+	case BOARD_MOVE.Right:
+		if currentIndex%boardSide == boardSide-1 {
+			return boardSide * boardSide
+		}
+
+		return currentIndex + 1
+	}
+
+	return currentIndex
+}
+
+func indexOf(array *[]int, value int) int {
+	var arrayLength = len(*array)
+	for i := 0; i < arrayLength; i++ {
+		if (*array)[i] == 0 {
+			return i
+		}
+	}
+
+	return -1
+}
+
+func getZeroIndex(array *[]int) int {
+	return indexOf(array, 0)
+}
+
+func getSuccessors(boardSide int, node Node) []Node {
+	fmt.Println("Node:", node.board)
+	var zeroIndex = getZeroIndex(&node.board)
+	fmt.Println("Zero index:", zeroIndex)
+	var boardLength = len(node.board)
+
+	var result []Node
+
+	for i := 0; i < 4; i++ {
+		var moveIndex = getMoveIndex(boardSide, zeroIndex, BoardMove(i))
+
+		fmt.Println("Move index: ", moveIndex)
+
+		if moveIndex >= 0 && moveIndex < boardLength {
+			fmt.Println("Valid Move index: ", moveIndex)
+			var board = make([]int, boardLength)
+			copy(board, node.board)
+			board[zeroIndex] = board[moveIndex]
+			board[moveIndex] = 0
+			result = append(result, Node{
+				0, // TODO: Use manhatten to calculate cost!
+				board,
+				list.List{},
+			})
+
+			fmt.Println("RES: ", result)
+		}
+	}
+
+	fmt.Println("Successors: ", result)
+
+	return result
+}
+
+type StepCost func(a *Node, b *Node) int
+
+func search(boardSide int, heuristic Heuristic, stepCost StepCost, isGoal GoalPredicate, path *[]*Node, currentCost int, threshold int) SearchResult {
+	var node = (*path)[len(*path)-1]
+	var cheapestPathCost = currentCost + heuristic(node)
+
+	if cheapestPathCost > threshold {
+		return SearchResult{cheapestPathCost, nil}
+	}
+
+	if isGoal(node) {
+		return SearchResult{cheapestPathCost, node}
+	}
+
+	var min = math.MaxInt
+
+	var successors = getSuccessors(boardSide, *node)
+
+	for i := 0; i < len(successors); i++ {
+		var successor = successors[i]
+
+		if !slices.Contains(*path, &successor) {
+			*path = append(*path, &successor)
+			var searchResult = search(boardSide, heuristic, stepCost, isGoal, path, currentCost+stepCost(node, &successor), threshold)
+
+			if searchResult.node != nil {
+				return searchResult
+			}
+
+			if searchResult.cost < min {
+				min = searchResult.cost
+			}
+
+			*path = (*path)[:len(*path)-1]
+		}
+	}
+
+	return SearchResult{min, nil}
+}
+
+type IDAResult struct {
+	path      list.List
+	threshold int
+}
+
+func idaStar(boardSide int, heuristic Heuristic, stepCost StepCost, isGoal GoalPredicate, root *Node) *IDAResult {
+	var threshold = heuristic(root)
+	var path = []*Node{root}
+
+	for {
+		var result = search(boardSide, heuristic, stepCost, isGoal, &path, 0, threshold)
+		if isGoal(result.node) {
+			fmt.Println("RESULT NODE:", result.node.board);
+			return &IDAResult{result.node.pathToNode, threshold}
+		}
+
+		if slices.Contains(path, result.node) {
+			return nil
+		}
+
+		threshold = result.cost
+	}
+}
+
+func isGoalCreator(goalIndex int) func(node *Node) bool {
+	isGoal := func(node *Node) bool {
+		if node == nil {
+			return false
+		}
+
+		return getZeroIndex(&((*node).board)) == goalIndex
+	}
+
+	return isGoal
+}
+
+func boardSizeToBoardSide(size int) int {
+	return int(math.Abs(math.Sqrt(float64(size + 1))))
+}
+
+func index1DToIndex2D(boardSide int, index int) Point2D {
+	var x int = index % boardSide
+	var y int = index / boardSide
+
+	return Point2D{x, y}
+}
+
+func boardToPoint2D(boardSide int, board *[]int) Point2D {
+	return index1DToIndex2D(boardSide, getZeroIndex(board))
 }
 
 func main() {
-	fmt.Println(getManhattenDistance(Point2D{1, 1}, Point2D{2, 2}))
+	var boardSize int = 8
+	var boardSide = boardSizeToBoardSide(boardSize)
+	var isGoal = isGoalCreator(8)
+	var board = []int{1, 2, 3, 4, 5, 6, 0, 7, 8}
+	var heuristic = func(node *Node) int {
+		return getManhattenDistance(boardToPoint2D(boardSide, &board), index1DToIndex2D(boardSide, boardSize))
+	}
+
+	var stepCost StepCost = func(a *Node, b *Node) int {
+		return getManhattenDistance(boardToPoint2D(boardSide, &(*a).board), boardToPoint2D(boardSide, &(*b).board))
+	}
+
+	var root = &Node{0, board, *list.New()}
+
+	var result = idaStar(boardSide, heuristic, stepCost, isGoal, root)
+
+	fmt.Println((*result).threshold)
 }
