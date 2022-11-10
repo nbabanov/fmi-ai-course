83d88966159ac78ef6e98185969a8106a4388482
Nikolay Babanov <ni.babanov@gmail.com>
2022-11-10 11:55:59 +0200

    HW2 Removed dead code

--- hw2-n-queens-rs/main.rs
+++ hw2-n-queens-rs/main.rs
@@ -1,15 +1,7 @@
 use rand::Rng;
 use std::{collections::HashMap, io, ops::Sub, time::Instant, vec};
 
-// const BOARD_SIZE_COEFF: u32 = 8000;
 const BOARD_SIZE_COEFF: u32 = 200;
-// const BOARD_SIZE_COEFF: u32 = 3;
-
-// _ 0 1 2
-// 0 _ _ _  [1, 3] -> [0, 1, 1, 1]
-// 1 Q _ _
-// 2 _ _ _
-// 3 _ Q _
 
 fn cell_coordinates_to_d1_key(column_index: &u32, row_index: &u32) -> i64 {
     i64::from(*column_index) - i64::from(*row_index)
@@ -75,12 +67,6 @@ fn get_conflicts_per_cell(
         .unwrap_or(u32::MAX);
 }
 
-#[derive(Debug)]
-struct Point2D {
-    x: u32,
-    y: u32
-}
-
 fn print_board(board_size: &u32, board: &Vec<u32>) {
     for row_index in 0..*board_size {
         for column_index in 0..*board_size {
@@ -107,70 +93,9 @@ fn initialize(
     let mut queens_per_d1: HashMap<i64, u32> = HashMap::new();
     let mut queens_per_d2: HashMap<i64, u32> = HashMap::new();
 
-    // let mut colnfl_per_column: Vec<Vec<u32>> = vec![];
-
-
-    // let static_data: Vec<Point2D> = vec![
-    //     // Point2D {
-    //     //     x: 0,
-    //     //     y: 0
-    //     // },
-    //     // Point2D {
-    //     //     x: 1,
-    //     //     y: 2
-    //     // },
-    //     // Point2D {
-    //     //     x: 2,
-    //     //     y: 0
-    //     // },
-    //     // Point2D {
-    //     //     x: 3,
-    //     //     y: 2
-    //     // }
-
-    //     Point2D {
-    //         x: 0,
-    //         y: 0
-    //     },
-    //     Point2D {
-    //         x: 1,
-    //         y: 0
-    //     },
-    //     Point2D {
-    //         x: 2,
-    //         y: 0
-    //     },
-    //     Point2D {
-    //         x: 3,
-    //         y: 0
-    //     }
-    // ];
-
-    // for point in static_data {
-    //     board.push(point.y);
-    //     let row_index = point.y;
-    //     let column_index = point.x;
-    //     queens_per_row[row_index as usize] += 1;
-    
-    //     let d1_key = cell_coordinates_to_d1_key(&column_index, &row_index);
-    //     let d1_value = queens_per_d1.get(&d1_key).unwrap_or(&0);
-    //     queens_per_d1.insert(d1_key, d1_value + 1);
-    
-    //     let d2_key = cell_coordinates_to_d2_key(&column_index, &row_index);
-    //     let d2_value = queens_per_d2.get(&d2_key).unwrap_or(&0);
-    //     queens_per_d2.insert(d2_key, d2_value + 1);
-    // }
-
-    // println!("Per row: {:?}", queens_per_row);
-
-    // return (queens_per_row, queens_per_d1, queens_per_d2);
-
     for column_index in 0..*board_size {
-        // let mut column_conflicts: Vec<u32> = vec![];
-
         if board.len() == 0 {
             let first = rng.gen_range(1..*board_size);
-            // println!("First is {}", &first);
             board.push(first);
         } else {
             let mut min_conflicts = u32::MAX;
@@ -192,15 +117,9 @@ fn initialize(
                 } else if conflicts == min_conflicts {
                     min_conflicts_ids.push(row_index);
                 }
-                // column_conflicts.push(conflicts);
-                // column_conflicts.push();
             }
 
-            // println!("Current conflicts: {:?}", &column_conflicts);
-            // colnfl_per_column.push(column_conflicts);
-
             let move_index = rng.gen_range(0..min_conflicts_ids.len());
-            // println!("Move index: {}", &move_index);
 
             board.push(min_conflicts_ids[move_index]);
         }
@@ -217,8 +136,6 @@ fn initialize(
         let d2_value = queens_per_d2.get(&d2_key).unwrap_or(&0);
         queens_per_d2.insert(d2_key, d2_value + 1);
     }
-
-    // println!("All conflicts: {:?}", colnfl_per_column);
 
     return (queens_per_row, queens_per_d1, queens_per_d2);
 }
@@ -285,7 +202,6 @@ fn get_row_with_min_conflicts(
         } else if conflicts == min_conflicts {
             min_conflicts_ids.push(row_index as u32);
         }
-        // column_conflicts.push();
     }
 
     let move_index = rng.gen_range(0..min_conflicts_ids.len());
@@ -298,13 +214,9 @@ fn solve(board_size: &u32) -> Option<Vec<u32>> {
     let (mut queens_per_row, mut queens_per_d1, mut queens_per_d2) =
         initialize(board_size, &mut board);
 
-    // println!("Initial board:");
-    // print_board(&board_size, &board);
-
     let mut has_conflicts = false;
 
-    for ind in 0..BOARD_SIZE_COEFF * board_size {
-        // println!("Try {}", ind);
+    for _ in 0..BOARD_SIZE_COEFF * board_size {
         // randomly if two or more
         let (max_conflicts, max_conflicts_column) = get_col_with_queen_with_max_conflicts(
             &board,
@@ -312,11 +224,6 @@ fn solve(board_size: &u32) -> Option<Vec<u32>> {
             &queens_per_d1,
             &queens_per_d2,
         );
-
-        // println!(
-        //     "Max conflicts {} in column {}",
-        //     max_conflicts, max_conflicts_column
-        // );
 
         if max_conflicts == 0 {
             return Some(board);
@@ -364,16 +271,10 @@ fn solve(board_size: &u32) -> Option<Vec<u32>> {
         let d2_value_after = queens_per_d2.get(&d2_key_after).unwrap_or(&0);
         let d2_value_new_after = d2_value_after.checked_add(1).unwrap_or(u32::MAX);
         queens_per_d2.insert(d2_key_after, d2_value_new_after);
-
-        // print_board(&board_size, &board);
     }
 
     if has_conflicts {
-        // println!("Has conflicts!");
-        // println!("Has conflicts:");
-        // print_board(&board_size, &board);
         return None;
-        // return solve(board_size);
     }
 
     return Some(board);
